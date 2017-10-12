24de27cfdbfab2db5072f3c0a2067a4e9836b401
Jason Miner <jminer@carroll.edu>
2017-10-12 11:05:45 -0600

    10/12/17
    Some Code Added

--- Card.java
+++ Card.java
@@ -1,5 +1,67 @@
 
 public class Card 
 {
+	public int 		rank;
+    public String 	suit;
+    public boolean  isFaceUp;
+    
+    /**
+     * Constructs a Card object
+     * @param newRank rank of the card
+     * @param newSuit suit of the card
+     */
+    public Card(int newRank, String newSuit)
+    {
+        rank	 = newRank;
+        suit 	 = newSuit;
+        isFaceUp = false;
+    }
+    
+    public int getRank()
+    {
+        return rank;
+    }
 
+    public String getSuit()
+    {
+        return suit;
+    }
+
+    public boolean isRed()
+    {
+        return (suit.equals("d") || suit.equals("h"));
+    }
+
+    public boolean isFaceUp()
+    {
+        return isFaceUp;
+    }
+
+    public void turnUp()
+    {
+        isFaceUp = true;
+    }
+
+    public void turnDown()
+    {
+        isFaceUp = false;
+    }
+
+    public String getFileName()
+    {
+        if (!isFaceUp)  
+        	return "back.jpg"; 
+        if (rank == 10) 
+        	return "t" + suit + ".jpg";
+        if (rank == 11) 
+        	return "j" + suit + ".jpg";
+        if (rank == 12) 
+        	return "q" + suit + ".jpg";
+        if (rank == 13) 
+        	return "k" + suit + ".jpg";
+        if (rank == 1)  
+        	return "a" + suit + ".jpg";
+        
+        return rank + suit + ".jpg";
+    }
 }
--- Solitaire.java
+++ Solitaire.java
@@ -1,5 +1,17 @@
 
 public class Solitaire
 {
-
+	private Stack<Card> MyDrawStack;
+	private Stack<Card> MyDiscardStack;
+	
+	private FoundationStack<Card>[] myFoundations;
+	private TableauStack<Card>[] myTableau;
+	
+	
+	
+	
+	
+	
+	
+	
 }
