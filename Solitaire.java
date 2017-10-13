33c385eca7bef71afb3de473588de87026c8cf7a
Jason Miner <jminer@carroll.edu>
2017-10-12 19:28:17 -0600

    10/10/17
    Solitaire Skeletion

--- Solitaire.java
+++ Solitaire.java
@@ -8,10 +8,47 @@ public class Solitaire
 	private TableauStack<Card>[] myTableau;
 	
 	
+	public Solitaire() { /**Enter Code*/ }
+
+	public Card getDrawCard() { /**Enter Code*/ }
+	 
+	public Card getDiscardCard() { /**Enter Code*/ }
+
+	public Card getFoundationCard() { /**Enter Code*/ }
+	 
+	public Stack<Card> getStack() { /**Enter Code*/ }
+	 
+	public Stack<Card> createDraw() { /**Enter Code*/ }
 	
+	public Stack<Card> removeFaceUpCards() { /**Enter Code*/ }
 	
+	public void deal() { /**Enter Code*/ }
+	 
+	public void dealOneCards() { /**Enter Code*/ }
+	 
+	public void dealThreeCards() { /**Enter Code*/ }
+	 
+	public void resetDrawStack() { /**Enter Code*/ }
+	 
+	public void DrawStackClicked() { /**Enter Code*/ }
+	 
+	public void DiscardStackClicked() { /**Enter Code*/ }
+	 
+	public void foundationStackClicked() { /**Enter Code*/ }
+	 
+	public void tableauStackClicked() { /**Enter Code*/ }
+	 
+	public void initializeStacks() { /**Enter Code*/ }
 	
+	public void isGameOver() { /**Enter Code*/ }
+	 
+	public void displayGameOverDialog() { /**Enter Code*/ }
+	 
+	public void addToStack() { /**Enter Code*/ }
 	
-	
-	
+	public boolean canAddToStack() { /**Enter Code*/ }
+	 
+	public boolean canAddToFoundation() { /**Enter Code*/ }
+	 
+
 }
